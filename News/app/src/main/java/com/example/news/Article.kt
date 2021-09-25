fd53c972582ecb93f063c99d9066b2b45d130237
Sayyed Aquib <72184419+SayyedAquib@users.noreply.github.com>
2021-09-25 23:32:51 +0530

    Delete News directory

--- News/app/src/main/java/com/example/news/Article.kt
+++
@@ -1,9 +0,0 @@
-package com.example.news
-
-data class Article(
-    val author: String,
-    val title: String,
-    val description: String,
-    val urlToImage: String,
-    val url: String
-)
--- News/app/src/main/java/com/example/news/News.kt
+++
@@ -1,3 +0,0 @@
-package com.example.news
-
-data class News(val totalResults: Int, val articles: List<Article>)
